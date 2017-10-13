cd3b58f20ec14dce5288a796e357e9eead45de19
Aoife Pearson <aoifejo.pearson@googlemail.com>
2017-10-13 12:38:24 +0100

    Bishop CLass

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -10,7 +10,80 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            List<Square> availableMoves = new List<Square>();
+            var currentSquare = board.FindPiece(this);
+            Square newPosition = new Square(currentSquare.Row, currentSquare.Col);
+
+            while (newPosition.Row < Board.BoardSize && newPosition.Col < Board.BoardSize)
+            {
+                if (newPosition != currentSquare)
+                {
+                    availableMoves.Add(newPosition);
+
+                }
+                newPosition = MoveRightForward(newPosition);
+
+            }
+            newPosition = new Square(currentSquare.Row, currentSquare.Col);
+            while (newPosition.Row < Board.BoardSize && newPosition.Col >= 0)
+            {
+                if (newPosition != currentSquare)
+                {
+                    availableMoves.Add(newPosition);
+
+                }
+                newPosition = MoveLeftForward(newPosition);
+
+            }
+            newPosition = new Square(currentSquare.Row, currentSquare.Col);
+            while (newPosition.Row >= 0 && newPosition.Col >= 0)
+            {
+                if (newPosition != currentSquare)
+                {
+                    availableMoves.Add(newPosition);
+
+                }
+                newPosition = MoveLeftBackward(newPosition);
+
+            }
+            newPosition = new Square(currentSquare.Row, currentSquare.Col);
+            while (newPosition.Row >= 0 && newPosition.Col < Board.BoardSize)
+            {
+                if (newPosition != currentSquare)
+                {
+                    availableMoves.Add(newPosition);
+
+                }
+                newPosition = MoveRightBackward(newPosition);
+
+            }
+
+            return availableMoves;
+        }
+
+        public Square MoveLeftForward(Square newPosition)
+        {
+            Square newestPosition = new Square(newPosition.Row + 1, newPosition.Col - 1);
+
+            return newestPosition;
+        }
+        public Square MoveLeftBackward(Square newPosition)
+        {
+            Square newestPosition = new Square(newPosition.Row - 1, newPosition.Col - 1);
+
+            return newestPosition;
+        }
+        public Square MoveRightForward(Square newPosition)
+        {
+            Square newestPosition = new Square(newPosition.Row + 1, newPosition.Col + 1);
+
+            return newestPosition;
+        }
+        public Square MoveRightBackward(Square newPosition)
+        {
+            Square newestPosition = new Square(newPosition.Row - 1, newPosition.Col + 1);
+
+            return newestPosition;
         }
     }
 }
