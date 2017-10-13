8bf84d151661b3dc6a4ee5120d4e7561df8c9cef
Aoife Pearson <aoifejo.pearson@googlemail.com>
2017-10-13 14:21:25 +0100

    Knight Rules

--- Chessington.GameEngine/Pieces/Knight.cs
+++ Chessington.GameEngine/Pieces/Knight.cs
@@ -10,7 +10,44 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            List<Square> availableMoves = new List<Square>();
+            var currentSquare = board.FindPiece(this);
+            if (currentSquare.Row + 2 < Board.BoardSize && currentSquare.Col - 1 >= 0)
+            {
+                availableMoves.Add(new Square(currentSquare.Row + 2, currentSquare.Col - 1));
+            }
+            if (currentSquare.Row + 2 < Board.BoardSize && currentSquare.Col + 1 < Board.BoardSize)
+            {
+                availableMoves.Add(new Square(currentSquare.Row + 2, currentSquare.Col + 1));
+            }
+            if (currentSquare.Row - 2 >= 0 && currentSquare.Col - 1 >= 0)
+            {
+                availableMoves.Add(new Square(currentSquare.Row - 2, currentSquare.Col - 1));
+            }
+            if (currentSquare.Row - 2 >= 0 && currentSquare.Col + 1 < Board.BoardSize)
+            {
+                availableMoves.Add(new Square(currentSquare.Row - 2, currentSquare.Col + 1));
+            }
+            if (currentSquare.Row + 1 < Board.BoardSize && currentSquare.Col - 2 >= 0)
+            {
+                availableMoves.Add(new Square(currentSquare.Row + 1, currentSquare.Col - 2));
+            }
+            if (currentSquare.Row - 1 >= 0 && currentSquare.Col - 2 >= 0)
+            {
+                availableMoves.Add(new Square(currentSquare.Row - 1, currentSquare.Col - 2));
+            }
+            if (currentSquare.Row - 1 >= 0 && currentSquare.Col + 2 < Board.BoardSize)
+            {
+                availableMoves.Add(new Square(currentSquare.Row - 1, currentSquare.Col + 2));
+            }
+            if (currentSquare.Row + 1 < Board.BoardSize && currentSquare.Col + 2 < Board.BoardSize)
+            {
+                availableMoves.Add(new Square(currentSquare.Row + 1, currentSquare.Col + 2));
+            }
+            return availableMoves;
+
         }
+
+
     }
 }
